99ddc1333bfc305542930dd2dced1ef43bff8797
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2023-11-05 19:40:34 +0100

    .

--- src/infrastructure/database/tables.ts
+++ src/infrastructure/database/tables.ts
@@ -1,65 +1,7 @@
 /* eslint-disable camelcase */
-import { DataTypes, Sequelize } from 'sequelize';
+import { DataTypes, Model, ModelStatic, Sequelize } from 'sequelize';
 
-export function createPlayerModel(sequelize: Sequelize): object {
-  const PlayerTable = sequelize.define(
-    'players',
-    {
-      id_player: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true
-      },
-
-      name: {
-        type: DataTypes.CHAR(50),
-        allowNull: false
-      },
-
-      total_plays: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-
-      total_wins: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      }
-    },
-    {
-      tableName: 'players',
-      indexes: [
-        {
-          fields: ['id_player'],
-          name: 'id_player_idx'
-        }
-      ],
-      engine: 'InnoDB'
-    }
-  );
-
-  //falta implementar la relacion entre las tablas:
-
-  // PlayerTable.hasMany(sequelize.models.game, {
-  //   foreignKey: 'player_id',
-  //   constraints: false, // Set to true if you want to enable ON DELETE and ON UPDATE actions
-  //   as: 'player'
-  // });
-
-  sequelize
-
-    .sync({ force: false }) // Set force to true to drop and recreate the table
-    .then(() => {
-      console.log('Table synced successfully');
-    })
-    .catch((error) => {
-      console.error('Error syncing table:', error);
-    });
-
-  return PlayerTable;
-}
-
-export function createGameModel(sequelize: Sequelize): object {
+export function createGameModel(sequelize: Sequelize): ModelStatic<Model> {
   const GameTable = sequelize.define(
     'games',
     {
@@ -96,7 +38,7 @@ export function createGameModel(sequelize: Sequelize): object {
         {
           unique: true,
           fields: ['id_game'],
-          name: 'id_game_UNIQUE'
+          name: 'id_play_UNIQUE'
         },
         {
           fields: ['player_id'],
@@ -106,23 +48,53 @@ export function createGameModel(sequelize: Sequelize): object {
       engine: 'InnoDB'
     }
   );
-  //falta implementar la relacion entre las tablas:
 
-  // GameTable.belongsTo(sequelize.models.players, {
+  GameTable.belongsTo(sequelize.models.players, {
+    foreignKey: 'player_id'
+  });
+
+  return GameTable;
+}
+
+export function createPlayerModel(sequelize: Sequelize): ModelStatic<Model> {
+  const PlayerTable = sequelize.define(
+    'players',
+    {
+      id_player: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: DataTypes.CHAR(50),
+        allowNull: false
+      },
+      total_plays: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      total_wins: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
+    },
+    {
+      tableName: 'players',
+      indexes: [
+        {
+          fields: ['id_player'],
+          name: 'id_player_idx'
+        }
+      ],
+      engine: 'InnoDB'
+    }
+  );
+  //falta implementar la relacion entre las tablas:
+  // PlayerTable.hasMany(sequelize.models.game, {
   //   foreignKey: 'player_id',
-  //   targetKey: 'id_player',
   //   constraints: false, // Set to true if you want to enable ON DELETE and ON UPDATE actions
   //   as: 'player'
   // });
 
-  sequelize
-    .sync({ force: false }) // Set force to true to drop and recreate the table
-    .then(() => {
-      console.log('Table synced successfully');
-    })
-    .catch((error) => {
-      console.error('Error syncing table:', error);
-    });
-
-  return GameTable;
+  return PlayerTable;
 }
