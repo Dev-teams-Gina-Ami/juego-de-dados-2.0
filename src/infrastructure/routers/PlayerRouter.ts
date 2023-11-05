db5d865f82c4d84bd6d8c39a9ada3684c235f8d2
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2023-11-05 21:51:09 +0100

    .

--- src/infrastructure/routers/PlayerRouter.ts
+++ src/infrastructure/routers/PlayerRouter.ts
@@ -1,28 +1,30 @@
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
-import { MPlayer } from '../../_domain/repositories/MPlayer';
+// import { MPlayer } from '../../_domain/repositories/MPlayer';
 
 import { playerController } from './dependencies';
 
 export const playerRouter = express.Router();
+
+playerRouter.get('/hello');
 
 playerRouter.post(
   '/:id/counter',
   playerController.execute.bind(playerController)
 );
 
-playerRouter.get('/:id', (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.params);
-  const { id } = req.params;
-  MPlayer.find({ id: id })
-    .then((player) => {
-      if (player) {
-        return res.json(player);
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((err: Error) => {
-      next(err);
-    });
-});
+// playerRouter.get('/:id', (req: Request, res: Response, next: NextFunction) => {
+//   console.log(req.params);
+//   const { id } = req.params;
+//   MPlayer.find({ id: id })
+//     .then((player) => {
+//       if (player) {
+//         return res.json(player);
+//       } else {
+//         res.status(404).end();
+//       }
+//     })
+//     .catch((err: Error) => {
+//       next(err);
+//     });
+// });
