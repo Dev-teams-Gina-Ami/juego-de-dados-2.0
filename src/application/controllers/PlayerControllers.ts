0e1a0a641b47d9360f877c08a9fa06ba5df39d3e
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2023-11-08 11:04:05 +0100

    trabajando en PlayerRoutes

--- src/application/controllers/PlayerControllers.ts
+++ src/application/controllers/PlayerControllers.ts
@@ -5,6 +5,40 @@ import { PlayerRepositoriesImpl } from '../../infrastructure/repositories/Player
 
 const PlayerRepositories = new PlayerRepositoriesImpl();
 
+export const createPlayer = (req: Request, res: Response) => {
+  PlayerRepositories.findAllPlayers().then(() => {
+    let name: string = req.params.name;
+    console.log(name);
+    let newPlayer = new Player(name);
+    try {
+      PlayerRepositories.createPlayer(newPlayer);
+    } catch (error) {
+      res.status(500).json('Unable to store the new player');
+    }
+  });
+};
+
+export const updatePlayer = async (req: Request, res: Response) => {
+  //let playerId: number = Number(req.params.id);
+  const { id } = req.params;
+  const newInfo = req.body;
+
+  const newPlayerInfo = {
+      name: newInfo.name,
+      totalPlays: newInfo.total_plays,
+      totalWins: newInfo.total_wins
+    };
+
+    try {
+      const updatePlayer = await Player.findByIdAndUpdate??
+    } catch (error) {
+      console.error('Error while retrieving data:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+
+
 export const getPlayer = (req: Request, res: Response) => {
   PlayerRepositories.findAllPlayers()
     .then((playersFound) => {
--- src/application/routes/PlayerRoutes.ts
+++ src/application/routes/PlayerRoutes.ts
@@ -1,6 +1,12 @@
 import * as express from 'express';
-import { getPlayer } from '../controllers/PlayerControllers';
+import {
+  getPlayer,
+  createPlayer,
+  updatePlayer
+} from '../controllers/PlayerControllers';
 
 export const playerRouter = express.Router();
 
+playerRouter.post('/', createPlayer);
+playerRouter.put('/:id', updatePlayer);
 playerRouter.get('/:id', getPlayer);
